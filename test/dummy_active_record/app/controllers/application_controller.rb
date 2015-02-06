6c960524be94e2358fa0fcc37cb592104f622943
Michael Johann <mjohann@rails-experts.com>
2015-02-06 13:22:48 +0100

    request protocol

--- test/dummy_active_record/app/controllers/application_controller.rb
+++ test/dummy_active_record/app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  helper :request_protocol
+
+  def request_protocol
+    Rails.env == 'development' ? 'http' : 'https'
+  end
 end
